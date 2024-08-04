5f9ee6e085c129eac8fd63198577c2c46b7eb129
Mahmut Safa Alıcı <72815188+Safalici@users.noreply.github.com>
2024-08-04 17:21:53 +0300

    Update check_email.py
    Updated to check if seeen the from headewr before, added index and count mechanism

--- check_email.py
+++ check_email.py
@@ -2,7 +2,7 @@ import imaplib
 import email
 from email.header import decode_header
 
-def check_inbox(username, password, imap_server):
+def check_inbox(username, password, imap_server, num_emails=10):
     try:
         # Connect to the server
         mail = imaplib.IMAP4_SSL(imap_server)
@@ -11,21 +11,38 @@ def check_inbox(username, password, imap_server):
         # Select the mailbox you want to check
         mail.select('inbox')
 
-        # Search for all emails in the inbox
+        # Fetch a larger batch of emails initially to ensure enough unique "From" headers
+        fetch_count = num_emails * 2
         status, messages = mail.search(None, 'ALL')
         email_ids = messages[0].split()
+        email_ids = email_ids[-fetch_count:]  # Get the most recent emails
 
-        for email_id in email_ids:
+        seen_from_headers = set()
+
+        index = 1
+        for email_id in reversed(email_ids):  # Process in reverse order to get most recent first
+            if index > num_emails:
+                break
+
             status, msg_data = mail.fetch(email_id, '(RFC822)')
             msg = email.message_from_bytes(msg_data[0][1])
 
-            # Print all headers
+            # Get and decode the "From" header
             from_header, encoding = decode_header(msg.get('From'))[0]
             if isinstance(from_header, bytes):
                 from_header = from_header.decode(encoding if encoding else 'utf-8')
-            print(f'From: {from_header}')
 
+            idnex2 = 0
+            if from_header in seen_from_headers:
+                print(f'{from_header} Görüldü:  {idnex2}')
+                idnex2 +=1 
+                continue
+
+            print(f'{index}. From: {from_header}')
             print('-------------------------------------------------------------')
+
+            seen_from_headers.add(from_header)
+            index += 1
 
         # Logout
         mail.logout()
@@ -34,17 +51,8 @@ def check_inbox(username, password, imap_server):
         print("An error occurred:", e)
 
 if __name__ == "__main__":
-    
     username = ''
     password = ''
-
-    imap_server = 'imap-mail.outlook.com' 
-
-    check_inbox(username, password, imap_server)
-
-
-##Gmail: imap.gmail.com
-##Outlook/Hotmail: imap-mail.outlook.com
-##Yahoo Mail: imap.mail.yahoo.com
-##iCloud Mail: imap.mail.me.com
-##AOL Mail: imap.aol.com
+    imap_server = 'imap-mail.outlook.com'  # Correct IMAP server for Hotmail/Outlook
+    num_emails = 10
+    check_inbox(username, password, imap_server, num_emails)
